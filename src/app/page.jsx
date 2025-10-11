e77294187efcf189b3b41db5ea159f2d7605ee00
CataniaDaavide <catania561@gmail.com>
2025-10-11 02:33:35 +0200

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,41 +6,60 @@ const config = {
   localStorage: {
     playerData: "PLAYERDATA",
   },
+  rowProperties:{
+    nomeGiocatore: { type: "text"},
+    prezzoBin: { type: "number"},
+    prezzoAcquistoAttuale: { type: "number"},
+    prezzoAcquistoMassimo: { type: "number", disabled: true },
+    prezzoVendita: { type: "number", disabled: true },
+    guadagnoNetto: { type: "number", disabled: true },
+  }
 };
 
-
 export default function App(props) {
-  const row = {
-    nomeGiocatore: { type: "text", value: "" },
-    prezzoBin: { type: "number", value: 0 },
-    prezzoAcquistoAttuale: { type: "number", value: 0},
-    prezzoAcquistoMassimo: { type: "number", value: "", disabled: true },
-    prezzoVendita: { type: "number", value: "", disabled: true },
-    guadagnoNetto: { type: "number", value: "", disabled: true },
+  const exampleRow = {
+    nomeGiocatore: "",
+    prezzoBin: 0,
+    prezzoAcquistoAttuale: 0,
+    prezzoAcquistoMassimo: 0,
+    prezzoVendita: 0,
+    guadagnoNetto: 0,
   };
+
 
   const [rows, setRows] = useState(
     Array(5)
       .fill()
-      .map(() => ({ ...row }))
+      .map(() => ({ ...exampleRow }))
   );
 
   // quandosi carica la pagina cerca dentro il localstorage i valori vecchi
   useEffect(() => {
+    //recupera i dati dal localstorage
     var playerData = localStorage.getItem(config.localStorage.playerData);
-    if (playerData) {
-      playerData = JSON.parse(playerData);
-      
-      if(playerData.length === 0) return;
 
-      setRows(playerData)
+    // se il playerData non è settato imposta un valore di default
+    if(!playerData){
+      localStorage.setItem(config.localStorage.playerData, JSON.stringify(rows));
+      return
     }
-  }, []);
 
+    // parse data
+    playerData = JSON.parse(playerData);
+
+    //se non ci sono elementi nell'array imposta un valore di default
+    if (playerData.length === 0) {
+        localStorage.setItem(config.localStorage.playerData, JSON.stringify(rows));
+        return;
+    };
+
+    //carica i data
+    setRows(playerData)
+  }, []);
 
   // click sul pulsante aggiungi riga
   const addRows = () => {
-    setRows((prev) => [...prev, row]);
+    setRows((prev) => [...prev, exampleRow]);
   };
 
   // click sul pulsante pulisci
@@ -48,12 +67,8 @@ export default function App(props) {
     const ris = confirm("Vuoi eliminare tutti i giocatori");
     if (!ris) return;
 
-    localStorage.setItem(config.localStorage.playerData, "[]");
-    setRows(
-      Array(5)
-        .fill()
-        .map(() => ({ ...row }))
-    );
+    localStorage.setItem(config.localStorage.playerData, JSON.stringify(Array(5).fill().map(() => ({ ...exampleRow }))));
+    window.location = "/"
   };
 
   return (
@@ -102,7 +117,6 @@ export default function App(props) {
 }
 
 function TableRow({ row, index }) {
-  
   // quando si esce dal foucus la funzione valuta il valore e imposta gli altri campi
   const handleValue = (e) => {
     // quando viene settato il valore aggiorna di conseguenza tutti gli altri campi della riga
@@ -116,7 +130,9 @@ function TableRow({ row, index }) {
       );
 
       //formula per calcolare il prezzo massimo
-      prezzoAcquistoMassimoInput.value = roundValue(prezzoBinVal - prezzoBinVal * 0.06);
+      prezzoAcquistoMassimoInput.value = roundValue(
+        prezzoBinVal - prezzoBinVal * 0.06
+      );
 
       // formula per calcolare il prezzo di vendita
       prezzoVenditaInput.value = roundValue(prezzoBinVal - prezzoBinVal * 0.01);
@@ -130,31 +146,48 @@ function TableRow({ row, index }) {
       );
       const prezzoVenditaVal = document.getElementById(
         `prezzoVendita_${index}`
-      )?.value ;
+      )?.value;
 
       // formula per calcolare il guadagno netto
-      guadagnoNettoVal.value = prezzoVenditaVal - (prezzoVenditaVal * 0.05) - prezzoAcquistoAttualeVal;
+      guadagnoNettoVal.value =
+        prezzoVenditaVal - prezzoVenditaVal * 0.05 - prezzoAcquistoAttualeVal;
     }
 
     // arrotonda i valori secondo le logiche di fc usando la funzione definita
     if (e.target.type != "text") {
       e.target.value = roundValue(e.target.value);
     }
+
+    // sincronizza i dati nel payload
+    var playerData = localStorage.getItem(config.localStorage.playerData);
+    playerData = JSON.parse(playerData);
+
+    playerData[index] = {
+      nomeGiocatore: document.getElementById(`nomeGiocatore_${index}`)?.value || "",
+      prezzoBin: document.getElementById(`prezzoBin_${index}`)?.value || 0,
+      prezzoAcquistoAttuale: document.getElementById(`prezzoAcquistoAttuale_${index}`)?.value || 0,
+      prezzoAcquistoMassimo:document.getElementById(`prezzoAcquistoMassimo_${index}`)?.value || 0,
+      prezzoVendita: document.getElementById(`prezzoVendita_${index}`)?.value || 0,
+      guadagnoNetto: document.getElementById(`guadagnoNetto_${index}`)?.value || 0,
+    };
+    localStorage.setItem(config.localStorage.playerData, JSON.stringify(playerData));
   };
 
   return (
     <tr>
       {Object.keys(row).map((key, i) => {
         const data = row[key];
+        const settings = config.rowProperties[key]
+        console.log(index ,settings, data)
         return (
           <td key={i} className="p-2">
             <input
               key={key}
-              type={data.type}
+              type={settings.type}
               id={`${key}_${index}`}
-              defaultValue={data.value}
+              defaultValue={data}
               onBlur={handleValue}
-              disabled={data.disabled}
+              disabled={settings.disabled}
               className={`w-full border border-gray-300 rounded px-2 py-1 focus:outline-none focus:ring-2 focus:ring-blue-400 disabled:bg-gray-300`}
             />
           </td>
